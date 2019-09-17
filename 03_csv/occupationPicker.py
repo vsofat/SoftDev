2b8f857d3299d5e3b19c8dd632dddf6a9a89eae2
vsofat <vsofat00@stuy.edu>
2019-09-17 06:11:43 -0400

    parsing and adding to dict

--- 03_csv/occupationPicker.py
+++ 03_csv/occupationPicker.py
@@ -4,10 +4,10 @@ import random
 # csv library makes reading the csv we have easier because there are "," in various spots and we can not simply split along the ","
 
 with open('occupations.csv') as csv_file:
-    reader = csv.reader(csv_file, delimiter = ",")
+    read = csv.reader(csv_file, delimiter = ",")
     dict = {}
     l = 0
-    for row in reader:
+    for row in read:
         if l == 0:
             dict[row[0]] = float(row[1]) * 10
             placeHolder = row[0]
@@ -18,16 +18,3 @@ with open('occupations.csv') as csv_file:
             l = l + 1
 
 print dict
-
-def randAvg():
-    total = 100 * random.random()
-    index = 0.0
-    for keys in dict:
-        if total < index + float(dict[keys]):
-            return (keys)
-        else:
-            index += float(dict[keys])
-
-print randAvg()
-print randAvg()
-print randAvg()
