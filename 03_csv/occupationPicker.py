7bf6156137d95ad1d215c4ebe676e7b54f13ed32
vsofat <vsofat00@stuy.edu>
2019-09-17 07:07:46 -0400

    combining my version and kiran's version

--- 03_csv/occupationPicker.py
+++ 03_csv/occupationPicker.py
@@ -4,17 +4,27 @@ import random
 # csv library makes reading the csv we have easier because there are "," in various spots and we can not simply split along the ","
 
 with open('occupations.csv') as csv_file:
+    #code by Vishwaa, edited by kiran
     read = csv.reader(csv_file, delimiter = ",")
     dict = {}
     l = 0
     for row in read:
+        # print row
         if l == 0:
-            dict[row[0]] = float(row[1]) * 10
-            placeHolder = row[0]
+            # skip header row
             l = l + 1
         else:
-            dict[row[0]] = (float(row[1]) * 10) + (dict[placeHolder])
-            placeHolder = row[0]
-            l = l + 1
+            dict[row[0]] = float(row[1]) / 100
 
-print dict
+# print dict
+
+def randomOccupation(dict):
+    randomVal = random.random() # random 0 to 1
+    percentageTracker = 0.0;
+    for key in dict:
+        # adds each percentage to a running total, meaning that theres a (percentage) chance the random value is newly underneath the total
+        percentageTracker = percentageTracker + dict[key]
+        if randomVal < percentageTracker:
+            return key
+
+print randomOccupation(dict)
