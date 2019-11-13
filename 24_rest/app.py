0b13866547bff7f8baacfa0cb34216f5289b7aaa
vsofat <vsofat00@stuy.edu>
2019-11-13 08:26:01 -0500

    hw

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -8,7 +8,7 @@ def root():
     url = urlopen("https://api.nasa.gov/planetary/apod?api_key=FGtm7aHTXQlrO7Gd3JWbEWA0e1cNT5hLKMETo2Mp")
     response = url.read()
     data = json.loads(response)
-    return render_template('yeet.html', imgsrc=data['url'], explanation = data['explanation'])
+    return render_template('nasa.html', imgsrc=data['url'], explanation = data['explanation'])
 
 if __name__ == "__main__":
     app.debug = True
