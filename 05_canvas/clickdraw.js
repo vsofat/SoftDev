4f72faa419eab6e727c125468b5cc3b725c40da2
vsofat <vsofat00@stuy.edu>
2020-02-12 12:11:13 -0500

    work work work

---
+++ 05_canvas/clickdraw.js
@@ -0,0 +1,54 @@
+var curr_state = "circle"
+
+
+const canvas = document.getElementById("mood");
+const button = document.getElementById("toggle");
+const status = document.getElementById("status");
+const clear = document.getElementById("clear");
+const ctx = canvas.getContext("2d");
+
+
+var toggle = function(e){
+	if (curr_state === "circle"){
+		curr_state = "rect";
+		status.innerHTML = "rectangle";
+	}
+
+	else{
+		curr_state = "circle";
+		status.innerHTML = "circle";
+
+	}
+
+	console.log("current state is");
+	console.log(curr_state);
+
+};
+
+var clearcanvas = function(canvas, contex){
+	context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+var draw = function (e, state){
+	var rect = canvas.getBoundingClientRect();
+	const x = e.clientX - rect.left; // offsets the xy
+	const y = e.clientY - rect.top;
+	if (e.buttons > 0){             // if the mouse button is down
+		if (state === "circle"){
+	                ctx.beginPath();
+	                ctx.arc(x, y, 5, 0, 2 * Math.PI);
+	                ctx.stroke();
+	        }
+	        else {
+	                ctx.beginPath();
+	                ctx.rect(x, y, 8, 8);
+			ctx.stroke();
+		}
+	}
+};
+
+
+// adding all the listeners
+button.addEventListener("click", toggle);
+clear.addEventListener("click", function(){ctx.clearRect(0, 0, canvas.width, canvas.height);});
+canvas.addEventListener("mousemove", function(e, state){draw(e, curr_state)});
