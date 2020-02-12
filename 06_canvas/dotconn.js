37c6d314e445b46aabe76cf720efdf6c8cc29235
vsofat <vsofat00@stuy.edu>
2020-02-12 13:05:26 -0500

    thank you, done

---
+++ 06_canvas/dotconn.js
@@ -0,0 +1,37 @@
+//Vishwaa Sofat & Ahmed Sultan -- #VIVACATALUNYA
+//Softdev pd09
+//K06 -- Dot Dot Dot
+//2020-02-12
+
+var canvas = document.getElementById("playground");
+var ctx = canvas.getContext("2d");
+
+var curmode = "dot";
+
+lx = null
+ly = null
+
+canvas.addEventListener("click", (e) => {
+    console.log("here");
+    currx = e.pageX - canvas.offsetLeft;
+    curry = e.pageY - canvas.offsetTop;
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(currx, curry, 10, 0, 2 * Math.PI);
+    ctx.fill();
+    if(lx != null && ly != null){
+      ctx.beginPath();
+      ctx.moveTo(lx, ly);
+      ctx.lineTo(currx, curry);
+      ctx.stroke();
+    }
+    lx = currx;
+    ly = curry;
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  lx = null;
+  ly = null;
+});
